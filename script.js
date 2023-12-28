b47f34de3499abc4ea3363891011e501978fa716
DevBaddy <warbaddy19+github@gmail.com>
2023-12-28 17:55:42 -0400

    Added theme switcher

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+document.addEventListener("DOMContentLoaded", initialize);
+
 const hamburgerIcon = document.getElementById("hamburger");
 const menuIcon = document.getElementById("menuIcon");
 const sidebar = document.getElementById("sidebar");
 
+const body = document.getElementById("body");
+const sun = document.getElementById("sun");
+const moon = document.getElementById("moon");
+
 hamburgerIcon.addEventListener("click", summonBurgerMenu);
 menuIcon.addEventListener("click", closeMenu);
+
+sun.addEventListener("click", toggleLightMode);
+moon.addEventListener("click", toggleDarkMode);
+
+function initialize() {
+  if (localStorage.getItem("theme") === "") {
+    localStorage.setItem("dark");
+
+    body.classList.remove("light");
+    body.classList.add("dark");
+
+    sun.style.display = "block";
+    moon.style.display = "none";
+  }
+
+  if (localStorage.getItem("theme") === "light") {
+    body.classList.remove("dark");
+    body.classList.add("light");
+
+    sun.style.display = "none";
+    moon.style.display = "block";
+  }
+
+  if (localStorage.getItem("theme") === "dark") {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  }
+}
 
 function summonBurgerMenu() {
   sidebar.style.display = "block";
@@ -11,4 +45,32 @@ function summonBurgerMenu() {
 
 function closeMenu() {
   sidebar.style.display = "none";
+}
+
+if (body.className === "light") {
+  sun.style.display = "none";
+}
+
+if (body.className === "dark") {
+  moon.style.display = "none";
+}
+
+function toggleLightMode() {
+  localStorage.setItem("theme", "light");
+
+  body.classList.remove("dark");
+  body.classList.add("light");
+
+  sun.style.display = "none";
+  moon.style.display = "block";
+}
+
+function toggleDarkMode() {
+  localStorage.setItem("theme", "dark");
+
+  body.classList.remove("light");
+  body.classList.add("dark");
+
+  sun.style.display = "block";
+  moon.style.display = "none";
 }
